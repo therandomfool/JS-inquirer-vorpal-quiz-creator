03c90092cdbca64e62ee62bec5129adaafd3a4d8
therandomfool <mddenver@protonmail.com>
2021-03-24 23:19:38 -0700

    first two test of random green

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,22 @@
 import fs from 'fs'
 
-export const chooseRandom = () => {
-  // TODO implement chooseRandom
+export const chooseRandom = (array, number) => {
+  let numItems 
+  let defArr = []
+  let newArray = []
+  defArr = array
+
+  if (defArr.length < 2){
+    return defArr;
+  }
+  if (number < 1 || number > defArr.length || number == undefined){
+   numItems = Math.floor(Math.random() * (array.length-1)+ 1); 
+  }
+
+  for(let i = 1; i < numItems; i++){
+    newArray.push(defArr[Math.floor(Math.random() * (array.length)+ 1)])
+  }
+  return newArray;
 }
 
 export const createPrompt = () => {
