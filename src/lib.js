a4e21351359a4a00f616a6820798b4da7c897b29
therandomfool <mddenver@protonmail.com>
2021-03-25 19:32:12 -0700

    Propmt working

--- src/lib.js
+++ src/lib.js
@@ -1,29 +1,51 @@
 import fs from 'fs'
 
 export const chooseRandom = (array, number) => {
-  let numItems 
+  let numItems
   let defArr = []
   let newArray = []
   defArr = array
 
-  if (defArr.length < 2){
+  if (defArr.length < 2) {
     return defArr;
   }
-  if (number < 1 || number > defArr.length || !number  ){
-   numItems = Math.floor(Math.random() * (array.length-1)+ 1); 
+  if (number < 1 || number > defArr.length || !number) {
+    numItems = Math.floor(Math.random() * (array.length - 1) + 1);
   } else {
     numItems = number;
   }
 
-  for(let i = 1; i <= numItems; i++){
-    newArray.push(defArr[Math.floor(Math.random() * (array.length)+ 1)])
+  for (let i = 1; i <= numItems; i++) {
+    newArray.push(defArr[Math.floor(Math.random() * (array.length) + 1)])
   }
-  
+
   return newArray;
 }
 
-export const createPrompt = () => {
-  // TODO implement createPrompt
+export const createPrompt = ({numQuestions = 1, numChoices = 2} = {}) => {
+  const promptArray = []
+
+  // console.log(`nQuestions: --> ${numQuestions}`)
+  // console.log(`nChoices: --> ${numChoices}`)
+
+  for (let q = 0; q < numQuestions; q++) {
+
+    promptArray.push({
+      type: `input`,
+      name: `question-${q+1}`,
+      message: `Enter question ${(q+1)}`
+    })
+
+    for (let c = 0; c < numChoices; c++) {
+      promptArray.push({
+        type: `input`,
+        name: `question-${(q+1)}-choice-${(c+1)}`,
+        message: `Enter answer choice ${(c+1)} for question ${(q+1)}`
+      })
+    }
+  }
+  // console.log(promptArray)
+  return promptArray;
 }
 
 export const createQuestions = () => {
